82da1b97de0ae95b4c35af0302be9e644ce7cf52
hurricaneabel <brunito777hurricane1@gmail.com>
2026-06-04 12:02:03 -0300

    MAP DRV EFFECT MODELS

--- matribox_sysex.py
+++ matribox_sysex.py
@@ -30,7 +30,40 @@ FREQ_EFFECTS = {
     (2, 15): "RING MOD",
     (3, 3): "TAPE MOD",
 }
-
+WAH_EFFECTS = {
+    (0, 1): "VOX WAH",
+    (0, 8): "CRY WAH",
+    (0, 10): "RACK WAH",
+    (0, 7): "BASS WAH",
+    (0, 15): "TOUCH WAH",
+    (1, 5): "AUTO WAH",
+}
+DRV_EFFECTS = {
+    (0, 0): "SKREAMER",
+    (0, 1): "SKREAMER 9",
+    (0, 2): "BUTTER OD",
+    (0, 4): "WARM OD",
+    (0, 6): "SUPER OD",
+    (0, 9): "BLUES OD",
+    (0, 10): "FULL OD",
+    (0, 14): "BREAKER OD",
+    (1, 0): "GERDEN OD",
+    (1, 14): "TIMMY OD",
+    (0, 15): "MASTER OD",
+    (2, 6): "SOLAR FUZZ",
+    (2, 2): "FUZZ CREAM",
+    (2, 4): "RED FUZZ",
+    (2, 10): "JP DIST",
+    (2, 11): "DARK MOUSE",
+    (2, 13): "PLEXI DIST",
+    (2, 14): "MASTER DIST",
+    (2, 9): "DIST PLUS",
+    (3, 0): "SHARK",
+    (3, 2): "STRIVE",
+    (5, 2): "SARDAR DIST",
+    (3, 15): "BASS OD",
+    (4, 0): "BASS DIST",
+}
 
 def decode_preset(data):
     if len(data) < 40:
@@ -70,14 +103,31 @@ def decode_effect_model(data):
             "effect_name": effect_name,
         }
 
-    # FREQ
+    # FREQ / WAH
     if len(data) == 128 and data[8] == 58:
         effect_key = (data[60], data[61])
-        effect_name = FREQ_EFFECTS.get(effect_key)
+        category_id = data[49]
+
+        if category_id == 1:
+            category = "FREQ"
+            effect_name = FREQ_EFFECTS.get(effect_key)
+
+        elif category_id == 2:
+            category = "WAH"
+            effect_name = WAH_EFFECTS.get(effect_key)
+
+        elif category_id == 3:
+            category = "DRV"
+            effect_name = DRV_EFFECTS.get(effect_key)
+
+        else:
+            category = f"UNKNOWN_CATEGORY_{category_id}"
+            effect_name = None
 
         return {
             "type": "effect_model",
-            "category": "FREQ",
+            "category": category,
+            "category_id": category_id,
             "effect_key": effect_key,
             "effect_name": effect_name,
         }
